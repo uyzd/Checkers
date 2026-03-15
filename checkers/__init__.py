ed0764a60a0f9db3e40ffc4a60f331f1a29a20e7
uyzd <trujillosimales1@gmail.com>
2026-03-15 17:53:38 +0100

    Actualizar __init__.py

--- checkers/__init__.py
+++ checkers/__init__.py
@@ -3,3 +3,21 @@ from .steam import check_steam_username
 from .roblox import check_roblox_username
 from .discord_check import check_discord_username
 from .discord import post_platform_discord, post_discord
+
+from .chess import check_chess_username
+from .discord_vanity import check_discord_vanity
+from .fortnite import check_fortnite_username
+from .github import check_github_username
+from .instagram import check_instagram_username
+from .kahoot import check_kahoot_username
+from .lichess import check_lichess_username
+from .minecraft import check_minecraft_username
+from .pastebin import check_pastebin_username
+from .replit_check import check_replit_username
+from .solo import check_solo_username
+from .soundcloud import check_soundcloud_username
+from .speedrun import check_speedrun_username
+from .tiktok import check_tiktok_username
+from .twitter import check_twitter_username
+from .youtube import check_youtube_username
+from .twitch import check_twitch_username
