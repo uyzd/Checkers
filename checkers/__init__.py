c41af8169ebd8a39e43c31e42ba490f9ec2774e0
uyzd <trujillosimales1@gmail.com>
2026-03-15 14:31:49 +0100

    Actualizar __init__.py

--- checkers/__init__.py
+++ checkers/__init__.py
@@ -1,4 +1,5 @@
 from .xbox import check_xbox_username
 from .steam import check_steam_username
 from .roblox import check_roblox_username
+from .discord_check import check_discord_username
 from .discord import post_platform_discord, post_discord
