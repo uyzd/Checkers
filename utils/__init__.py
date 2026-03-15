89b4c9af689fe154b0ddb38f3bc1e2a303f18881
uyzd <trujillosimales1@gmail.com>
2026-03-15 14:32:39 +0100

    Actualizar __init__.py
    Help

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,11 @@
-from .generator import generate_username
-from .ui import ask, ask_yn, print_banner
+from .generator import (
+    generate_username,
+    generate_random,
+    generate_4l,
+    generate_4c,
+    generate_5n,
+    generate_word,
+    generate_semi,
+)
+from .platforms import allowed_special, semi_supported
+from .ui import ask, ask_yn, ask_choice, print_banner
